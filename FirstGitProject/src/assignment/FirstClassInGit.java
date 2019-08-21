b644806e079ef977a6946d9b975bf07cc117b60d
dp05063 <dp05063@CEIT2553214X023.ad.georgiasouthern.edu>
2019-08-21 16:35:07 -0400

    Changed statement try 1

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -3,8 +3,8 @@ package assignment;
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		
+		System.out.println("hello my name is Dhruv!");
+		System.out.println("This is Csci 1302");
 		//add a statement to display your name in the console
 		
 		//Then commit your changes
