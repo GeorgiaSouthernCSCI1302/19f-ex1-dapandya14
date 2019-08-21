086624f1dd120b203f78bb0aafc0174f0ce1a05e
dp05063 <dp05063@CEIT2553214X023.ad.georgiasouthern.edu>
2019-08-21 16:54:30 -0400

    Added lastname

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,14 +1,17 @@
 package assignment;
-
+import java.util.Scanner;
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
-		System.out.println("hello my name is Dhruv!");
-		System.out.println("This is Csci 1302");
-		//add a statement to display your name in the console
 		
+		//add a statement to display your name in the console
+		String lastname;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the lastname: ");
+		lastname = input.nextLine();  
 		//Then commit your changes
-
+		
+		System.out.println("My name is Dhruv " + lastname);
 	}
 
 }
